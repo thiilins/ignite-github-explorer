54a21d624063f67627d3f3ed7dab997b44f64be0
Thiago Lins <taglins@gmail.com>
2021-11-03 05:48:41 -0300

    Finalizando a Aplicação

--- src/components/RepositoryList.tsx
+++ src/components/RepositoryList.tsx
@@ -1,10 +1,13 @@
 import { RepositoryItem } from "./RepositoryItem";
 import {useState, useEffect} from 'react'
 import '../styles/repositories.scss';
-
-const repositoryName = 'unform'
+interface Repository{
+    name: string;
+    description: string;
+    html_url: string;
+}
 export function RepositoryList() {
-const [repositories, setRepositories] = useState([])
+const [repositories, setRepositories] = useState<Repository[]>([])
 useEffect(()=>{
   fetch('https://api.github.com/orgs/rocketseat/repos')
   .then(response=>response.json()
@@ -14,7 +17,6 @@ useEffect(()=>{
   return (
     <section className="repository-list">
       <h1> Lista de repositórios</h1>
-
       <ul>
         {repositories.map(repository => <RepositoryItem key={repository.name} repository={repository} />)}
       </ul>
